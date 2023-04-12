7d3af2af11a947887c6785eaad9996453325ecca
arzoo.shaikh <arzoo.rkcet@gmail.com>
2023-04-12 12:51:31 +0530

    initial update with fetch all blogs

---
+++ src/components/Home.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+const Home = () => {
+    const [blogs,setBlogs] = useState([{"_id":"","title":"","description":""}])
+    // to call API Once
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    const fetchData = async  ()=>{
+        const response = await fetch('http://localhost:5000/api/blog/')
+        const jsonData = await response.json()
+        console.log(jsonData);
+        // console.log(jsonData.blogs)
+        setBlogs(jsonData['blogs'])
+        console.log(blogs)
+    }
+
+  return (
+    <div>{blogs && <table><thead>
+        <tr>
+            <th>Sr. No</th>
+            <th>Title</th>
+            <th>Description</th>
+            </tr></thead><tbody>{ blogs.map((blog,index) => (
+        <tr key={blog._id}>
+            <td>{index+1}</td>
+            <td>{blog.title}</td>
+            <td> {blog.description}</td></tr>
+       
+   ))}</tbody></table>
+       
+    }</div>
+  )
+}
+
+export default Home
